9e46ba74e1e3e62e088dca3638d8db270385da3b
Raman Novikau <demoikk@gmail.com>
2021-01-07 19:25:32 +0300

    feat: add bootstrap import

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // Require  html-webpack-plugin plugin
 module.exports = {
-  entry: `${__dirname}/js/index.js`, // webpack entry point. Module to start building dependency graph
+  entry: `${__dirname}/src/index.js`, // webpack entry point. Module to start building dependency graph
   output: {
     path: `${__dirname}/dist`, // Folder to store generated bundle
     filename: 'bundle.js', // Name of generated bundle after build
-    publicPath: '/', // public URL of the output directory when referenced in a browser
   },
   module: { // where we defined file patterns and their loaders
     rules: [
@@ -43,6 +42,7 @@ module.exports = {
   },
   plugins: [ // Array of plugins to apply to build chunk
     new HtmlWebpackPlugin({
+      filename: 'index.html',
       template: `${__dirname}/index.html`,
       inject: 'body',
     }),
