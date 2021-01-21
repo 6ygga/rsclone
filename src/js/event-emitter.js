cfdafa023d8e929e493e67eac8c723572c9f23e3
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-01-21 09:35:53 +0300

    feat: add event-emitter.js & install babel-eslint

---
+++ src/js/event-emitter.js
@@ -0,0 +1,16 @@
+export default class EventEmitter {
+    #events;
+
+    constructor() {
+      this.#events = {};
+    }
+
+    on(event, listener) {
+      (this.#events[event] || (this.#events[event] = [])).push(listener);
+      return this;
+    }
+
+    emit(event, argument) {
+      (this.#events[event] || []).slice().forEach((listener) => listener(argument));
+    }
+}
