8508a39555befd848b5720739e97411907dd9b6c
Evgeniy Marinovich <zmarinovich@mail.ru>
2021-02-02 14:41:50 +0300

    fix: path favicon

--- src/js/english/english-statistics/statistics-view.js
+++ src/js/english/english-statistics/statistics-view.js
@@ -27,6 +27,7 @@ export default class Statistics {
           this.sortTable();
         }, 500);
       }).catch((e) => {
+        /* eslint-disable-next-line */
         console.log(e);
       });
     }
