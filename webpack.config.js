03ef862f7d0f6e8b0e85f8cd0feacab0707a4704
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-02-03 12:26:25 +0300

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// Require  html-webpack-plugin plugin
+
 module.exports = {
-  entry: `${__dirname}/src/js/index.js`, // webpack entry point. Module to start building dependency graph
+  mode: 'development',
+
+  entry: `${__dirname}/src/js/index.js`,
+
   output: {
-    path: `${__dirname}/dist`, // Folder to store generated bundle
-    filename: 'bundle.js', // Name of generated bundle after build
-    // publicPath: '/', // public URL of the output directory when referenced in a browser
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
   },
-  module: { // where we defined file patterns and their loaders
+
+  devServer: {
+    contentBase: './public',
+    open: true,
+    port: 7700,
+  },
+
+  module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      /* {
-        test: /\.(jpg|mp3|svg|png)$/,
-        loader: 'file-loader',
-        options: {
-          name: `./src/assets/images/[name].[ext]`,
-        },
-      }, */
       {
         enforce: 'pre',
         test: /\.js$/,
@@ -44,22 +31,21 @@ module.exports = {
       },
     ],
   },
-  plugins: [ // Array of plugins to apply to build chunk
+
+  plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: `${__dirname}/index.html`,
       inject: 'body',
       favicon: path.resolve(__dirname, 'src/assets/images/favicon.ico')
     }),
-    // new FaviconsWebpackPlugin(`${__dirname}/src/assets/images/favicon.ico`),
+
     new CopyWebpackPlugin({
       patterns: [
         { from: "./src/assets/images",
           to: "assets/images",
           globOptions: {
-            ignore: [
-              '**/*.ico',
-            ],
+            ignore: ['**/*.ico'],
           },
            },
         { from: "./src/assets/music",
@@ -68,10 +54,4 @@ module.exports = {
       ],
     }),
   ],
-  devServer: { // configuration for webpack-dev-server
-    contentBase: './public', // source of static assets
-    open: true,
-    port: 7700, // port to run dev-server
-  },
-
 };
