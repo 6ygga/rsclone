f6a551d74d10af3e3439e4f9518f1a589d3fa0ce
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-02-03 10:57:07 +0300

    refactor: index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import EnglishMathApp from './english-math-app/english-math-app';
-import '../scss/index.scss';
 import 'bootstrap';
+import '../scss/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import EnglishMathApp from './english-math-app/english-math-app';
 import MainPage from './main-page/main-page';
 import UserAuth from './user-auth/user-auth';
 
